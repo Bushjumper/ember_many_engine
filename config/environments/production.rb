23c587b42c7a4ae0df5dc76fa4473affc026981c
Graeme Longman <graemelongman@gmail.com>
2015-10-05 10:37:18 +0200

    Adds firebase config to production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.firebase = Firebase::Client.new("https://" + 'ember-many-three' + ".firebaseio.com")
 end
